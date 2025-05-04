ad3adb0780b9d2a2e6df32913ae4f10935091914
myfruitmango <ctrl.fruitman90@gmail.com>
2025-05-05 03:27:36 +0700

    feat[error]: add doc & clear code

--- src/pages/error/index.jsx
+++ src/pages/error/index.jsx
@@ -1,13 +1,23 @@
+// ** React Imports
 import React, { useEffect, useRef } from 'react'
+
+// ** Howler Imports
 import { Howl } from 'howler'
+
+// ** Frame Imports
 import Frame from 'components/frame/error'
+
+// ** Assets Imports
 import Backsound from 'assets/backsound.mp3'
-import LoveSong from 'assets/music.mp3'
+import music from 'assets/music.mp3'
 
 const ErrorPage = () => {
+	// ! hooks
+	// * ref
 	const backsoundRef = useRef(null)
 	const loveSoundRef = useRef(null)
 
+	// * hooks
 	useEffect(() => {
 		backsoundRef.current = new Howl({
 			src: [Backsound],
@@ -21,11 +31,12 @@ const ErrorPage = () => {
 		}
 	}, [])
 
+	// ! handle
 	const handleSuccess = () => {
 		backsoundRef.current?.stop()
 
 		loveSoundRef.current = new Howl({
-			src: [LoveSong],
+			src: [music],
 			loop: true,
 			volume: 0.5
 		})
