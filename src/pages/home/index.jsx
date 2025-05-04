eb2e64b5666e6ac5eed47d620c8433e73d3dbcf0
myfruitmango <ctrl.fruitman90@gmail.com>
2025-05-05 06:38:10 +0700

    feat[home]: fixing page home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,54 @@
 // ** React Imports
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+
+// ** Howler Imports
+import { Howl } from 'howler'
 
 // ** Frame Imports
 import Frame from 'components/frame/home'
 
+// ** Assets Imports
+import Backsound from 'assets/backsound.mp3'
+import music from 'assets/music.mp3'
+
 const HomePage = () => {
-	return <Frame />
+	// ! hooks
+	// * ref
+	const backsoundRef = useRef(null)
+	const loveSoundRef = useRef(null)
+
+	// * hooks
+	useEffect(() => {
+		backsoundRef.current = new Howl({
+			src: [Backsound],
+			loop: true,
+			volume: 0.5
+		})
+
+		return () => {
+			backsoundRef.current?.unload()
+			loveSoundRef.current?.unload()
+		}
+	}, [])
+
+	// ! handle
+	const handleSuccess = () => {
+		backsoundRef.current?.stop()
+
+		loveSoundRef.current = new Howl({
+			src: [music],
+			loop: true,
+			volume: 0.5
+		})
+
+		loveSoundRef.current.play()
+	}
+
+	const handlePlay = () => {
+		backsoundRef.current?.play()
+	}
+
+	return <Frame onSuccess={handleSuccess} onPlay={handlePlay} />
 }
 
 export default HomePage
