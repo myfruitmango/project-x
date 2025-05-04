bcc536c9ccd83c3dcbe59bf72d4de656c0a6d4e9
myfruitmango <ctrl.fruitman90@gmail.com>
2025-05-05 02:30:53 +0700

    remove file not works!

--- src/config/enums/api-urls.js
+++
@@ -1,3 +0,0 @@
-const API_URLS = {}
-
-export default API_URLS
--- src/config/store/reducers.js
+++
@@ -1,6 +0,0 @@
-// ** Redux Imports
-import { combineReducers } from 'redux'
-
-// ** Modules Imports
-
-export const rootReducer = combineReducers({})
